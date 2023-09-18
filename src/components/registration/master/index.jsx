c444ada0d776282abcceb524d03f84a689263955
Andrei Adnolka <andrei.adnolka@schibsted.com>
2023-09-18 10:03:17 +0300

    modified:   src/components/registration/list/style.scss 	modified:   src/components/registration/master/index.jsx 	modified:   src/components/registration/master/style.scss

--- src/components/registration/master/index.jsx
+++ src/components/registration/master/index.jsx
@@ -1,24 +1,13 @@
 import "./style.scss";
 
-// id: "222",
-// name: "Леша",
-// place: "Октябрьская 21, барбершоп MLF_HUNTER",
-// number: "+375441111111",
-// servicesList: [
-//   { id: 1, label: "Срижка", price: "45 BYN", time: "1 ч" },
-//   { id: 2, label: "Срижка + борода", price: "55 BYN", time: "1 ч 30 мин" },
-// ],
-// ],
-
 const MasterUI = ({ name, servicesList, place, phone }) => {
   console.log("servicesList", servicesList);
   return (
     <div className="master_wrapper">
-      <div>Мастер: {name}</div>
-      <div>{place}</div>
-      {/* <a className="master__phone" href={`tel:${phone}`}>
-        Телефон мастера: {phone}
-      </a> */}
+      <div className="master_info">
+        Мастер: <span>{name}</span>
+        <div>{place}</div>
+      </div>
       <div className="master_services">
         <div className="master_services__title">Выберите услугу</div>
         <div className="master_services__list">
